596fcad18616290f83e2044aab95bdf97661f5d0
Nerusin Sergey <s.nerusin@yandex.ru>
2022-05-06 18:19:00 +0300

    Рефакторинг

--- js/main.js
+++ js/main.js
@@ -1,5 +1,33 @@
 /* jshint esversion: 6 */
 
+/* Объект с данными карточек для вывода при открытии сайта по умолчанию */
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
 /*  Редактирование профиля  */
 
 // кнопка редактирования профиля
@@ -28,89 +56,111 @@ const elementCard = document.querySelector('.cards');
 // определяем селектор откуда берём HTML разметку
 const template = document.querySelector('#oneCard').content;
 
+
+
+/* селекторы исполюзуемые в фукциях */
+
+// селектор поля - имя профиля
+const nameInput = document.querySelector('.form-profile__item_el_name');
+// селектор - поля профессия
+const jobInput = document.querySelector('.form-profile__item_el_profession');
+// селекторы профайла
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
+
+// селектор поля ввода: информация о фото
+const nameCardInput = document.querySelector('.form-card__item_el_name');
+// селектор поля ввода: ссылка на фото материал
+const linkCardInput = document.querySelector('.form-card__item_el_link');
+
+// определяем селектор модалного окна показа фото
+const imageModal = document.querySelector('.popup__card-image');
+
+
 /* ФУНКЦИИ */
 
 /* 1. Функция ввода данных из формы профайла */
-function formSubmitHandler (evt) {
+function submitProfileform (evt) {
   evt.preventDefault();
-  // селектор поля - имя профиля
-  const nameInput = document.querySelector('.form-profile__item_el_name');
-  // селектор - поля профессия
-  const jobInput = document.querySelector('.form-profile__item_el_profession');
-  const profileName = document.querySelector('.profile__name');
-  const profileProfession = document.querySelector('.profile__profession');
   profileName.textContent = nameInput.value;
   profileProfession.textContent = jobInput.value;
-  popupOpenClose(popupModalProfile);
+  togglePopup(popupModalProfile);
 }
 
 /* 2.Функция ввода данных из формы для добавления карточки */
-function formAddcardHandler (evt) {
+function submitAddcard (evt) {
   evt.preventDefault();
-  const nameCardInput = document.querySelector('.form-card__item_el_name');
-  // класс - поля профессия
-  const linkCardInput = document.querySelector('.form-card__item_el_link');
   // передаем в функцию ссылку на изображение, название карточки
-  addCards(linkCardInput.value, nameCardInput.value);
+  const oneCardElement = createCard(linkCardInput.value, nameCardInput.value);
+  addCard(oneCardElement);
   // очищаем поля ввода после отрисовки
   nameCardInput.value = '';
   linkCardInput.value = '';
   // закрываем окно после нажатия кнопки сохранить
-  popupOpenClose(popupModalCard);
+  togglePopup(popupModalCard);
 }
 
 /* 3. Функция открытия - закрытия модальных окон */
-function popupOpenClose(popupWindow){
+function togglePopup(popupWindow){
   popupWindow.classList.toggle('popup_opened');
 }
 
-/* 4. Функция формирования карточки с изображением, названием, лайком */
-function addCards(cardLink, cardName) {
+/* 4.1. Функция формирования карточки с изображением, названием, лайком */
+function createCard(cardLink, cardName) {
   // создаём копию  карточки HTML разметки
   const oneCard = template.querySelector('.cards__item').cloneNode(true);
+  // записываем в переменную селектор для:
+  // фото карточки
+  const selectorImage = oneCard.querySelector('.cards__image');
+  // корзинки удаления карточки
+  const selectorTrash = oneCard.querySelector('.cards__trash');
+  // всплытия подсказки
+  const selectorLike = oneCard.querySelector('.cards__like');
+  // названия карточки
+  const selectorTitle = oneCard.querySelector('.cards__title');
   // записываем  в атрибуты необходимые данные
-  oneCard.querySelector('.cards__image').src = cardLink;
-  oneCard.querySelector('.cards__image').alt = `Изображение: ${cardName}`;
-  oneCard.querySelector('.cards__trash').alt ='Изображение: Корзина';
-  oneCard.querySelector('.cards__trash').title ='Удалить карточку';
-  oneCard.querySelector('.cards__like').title = 'Поставить отметку: Нравится';
-  oneCard.querySelector('.cards__trash').src = './images/trash.svg';
-  oneCard.querySelector('.cards__title').textContent = cardName;
-  // добавляем карточку в HTML  разметку
-  elementCard.prepend(oneCard);
+  selectorImage.src = cardLink;
+  selectorImage.alt = `Изображение: ${cardName}`;
+  selectorTrash.alt ='Изображение: Корзина';
+  selectorTrash.title ='Удалить карточку';
+  selectorTrash.src = './images/trash.svg';
+  selectorLike.title = 'Поставить отметку: Нравится';
+  selectorTitle.textContent = cardName;
   // для установки прослушивателя на создаваемой карточке:
   // нажатие на сердечке - поставить like
-  const cardLike = document.querySelector('.cards__like');
-  putLikeHandler(cardLike);
+  putLikeHandler(selectorLike);
   // для установки прослушивателя на создаваемой карточке:
   // нажатие на корзинке - удалить карточку
-  const trashCard = document.querySelector('.cards__trash');
-  deleteCardHandler(trashCard);
-  // для установки прослушивателя на отрытие изображения из карточки
-  const imageCard = document.querySelector('.cards__image');
-  ViewImagesHandler(imageCard);
+  deleteCardHandler(selectorTrash);
+  // для установки прослушивателя на отркытие изображения из карточки
+  viewImagesfoto(selectorImage);
+  return oneCard;
+}
+
+/* 4.2. Функция добавления карточки в HTML разметку */
+function addCard(oneCard){
+  // добавляем карточку в HTML разметку
+  elementCard.prepend(oneCard);
 }
 
 /* 5. Функция: поставить отметку "Нравится" в карточке */
 function putLikeHandler(cardLike) {
  // устанавливаем прослушиватель на сердечко в карточке
-cardLike.addEventListener('click', function (evt) {
-  evt.target.classList.add('cards__like_active');});
+  cardLike.addEventListener('click', function (evt) {
+     evt.target.classList.toggle('cards__like_active');});
 }
 
 /* 6. Функция удаления карточки при нажатии на картинку */
 function deleteCardHandler(trashCard) {
   // устанавливаем прослушиватель на изображение корзины
   trashCard.addEventListener('click', function(evt) {
-      const card = evt.target.closest('.cards__item');
-      card.remove();});
+    const card = evt.target.closest('.cards__item');
+    card.remove();});
 }
 
 
 /* 7. Функция открытия картинки из  отрисованной карточки */
-function popupOpenImage(linkImage, titleImage, altImage){
-  // определяем селектор модалного окна показа фото
-  const imageModal = document.querySelector('.popup__card-image');
+function openFotofromCard(linkImage, titleImage, altImage){
   // затемняем фон для просмотра фото
   imageModal.style = 'background-color: #000';
   // устанавливаем необходимые атрибуты для отрисовки
@@ -119,76 +169,54 @@ function popupOpenImage(linkImage, titleImage, altImage){
   imageModal.querySelector('.popup__image').alt = altImage;
   // наименование фото
   imageModal.querySelector('.popup__image-caption').textContent = titleImage;
-  popupOpenClose(imageModal);
+  togglePopup(imageModal);
 }
 
 /* 8. Функция установки прослушивателя нажатия на кртинку карточки */
-function ViewImagesHandler(imageCard){
+function viewImagesfoto(imageCard){
   imageCard.addEventListener('click', function(evt) {
     const card = evt.target.closest('.cards__item');
     const linkImage = card.querySelector('.cards__image').getAttribute('src');
     const altImage = card.querySelector('.cards__image').getAttribute('alt');
     const titleImage = card.querySelector('.cards__title').textContent;
     // вызов функции открытия окна просмотра фото карточки
-    popupOpenImage(linkImage, titleImage, altImage);
+    openFotofromCard(linkImage, titleImage, altImage);
   });
 }
 
 /* Установка прослушивателей и отрисовка карточек из заданного обеъкта по умолчанию */
 
 // ставим прослушиватель на форму профайла
-formEditProfile.addEventListener('submit', formSubmitHandler);
+formEditProfile.addEventListener('submit', submitProfileform);
 // ставим прослушиватель на форму добавления карточки
-formEditCard.addEventListener('submit', formAddcardHandler);
+formEditCard.addEventListener('submit', submitAddcard);
 
 
 // ставим прослушиватель на кнопку открытия модального окна редактирования профайла
 buttonProfileEdit.addEventListener('click', function() {
-  popupOpenClose(popupModalProfile);
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileProfession.textContent;
+  togglePopup(popupModalProfile);
 });
 
 // ставим прослушиватель на кнопку открытия модального окна добавления фото
 buttonAddFoto.addEventListener('click', function() {
-  popupOpenClose(popupModalCard);
+  togglePopup(popupModalCard);
 });
 
 // Закрытие модальных окон
 popupCloseModal.forEach(function(closeModal) {
   const buttonClose = closeModal.closest('.popup');
-  closeModal.addEventListener('click', () => popupOpenClose(buttonClose));
+  closeModal.addEventListener('click', () => {
+    nameCardInput.value="";
+    linkCardInput.value="";
+    togglePopup(buttonClose);});
 });
-
-
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 
 // выводим  карточки из объекта initialCards
 initialCards.forEach(function(element){
-  addCards(element.link, element.name);
+  const oneCardElement = createCard(element.link, element.name);
+  addCard(oneCardElement);
 });
 
 
