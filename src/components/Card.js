f9902c645e3b54b28cd803c571e3e71e4ea3cef2
Nerusin Sergey <s.nerusin@yandex.ru>
2022-08-16 07:37:35 +0300

    feat: наброски классов 4

--- src/components/Card.js
+++ src/components/Card.js
@@ -16,7 +16,6 @@
     функция должна открывать попап с картинкой.
 */
 
-
 export default class Card{
   constructor(dataCard, personId, trash, cardSelectorTemplate, { handleClickLikeCard }, {handleImagezoomCardClick}, {openPopupConfirmCardDeleting}){
     this._name = dataCard.name; // имя будущей карточки
@@ -24,10 +23,10 @@ export default class Card{
     this._id = dataCard._id; // id будущей карточки
     this._trash = trash; // картинка корзинки - удаление карточки
     this._ownerId = dataCard.owner._id; // владелец карточки
-    this._likes = dataCard.likes; // получаем обект в котором инф-я кто лайкнул карточку
+    this._likes = dataCard.likes; // получаем обект в котором инфо о тех кто лайкнул карточку
     this._personId = personId; // мой id пользователя
     this._selectorTemplate = cardSelectorTemplate; // селектор разметки templete тега
-    this._handleCardClick = handleImagezoomCardClick; // развернуть картинку карточки при клике по ней
+    this._handleOpenImgCardClick = handleImagezoomCardClick; // развернуть картинку карточки при клике по ней
     this._handleClickLikeCard = handleClickLikeCard; // поставить-убрать лайк карточке
     this._popupConfirmDelete =  openPopupConfirmCardDeleting; // мод. окно подтверждения согласия на удаление карточки
   }
@@ -45,68 +44,60 @@ export default class Card{
     this._selectorLike = this._card.querySelector('.cards__like');
     this._selectorCountLike = this._card.querySelector('.cards__like-count');
     this._selectorTitle = this._card.querySelector('.cards__title');
-    this._card.id = this._id;
+    this._cardId = this._id;
     this._selectorImage.src = this.link;
-    this._selectorImage.alt = `Изображение: ${data.name}`;
-    if(this._ownerId === this._personId){
-      this._selectorTrash.alt ='Изображение: Корзина';
-      this._selectorTrash.title ='Удалить карточку';
-      this._selectorTrash.src = this.trash;
-      this._setEventCardTrash(this.card, this._selectorTrash, this._card._id); // вешаем прослушиватель на удаления карточки
-    } else {
-    this.selectorTrash._remove();  // если карточка не наша - удаляем элемент корзинки из html разметки карточки
-    }
-    for(let i=0; i < data.likes.length; i++){
-      if(data.likes[i]._id === personId){
-        this._selectorLike.classList.add('cards__like_active'); // если ранее ставили лайк на карточке - ставим лайк
-      }
-    }
+    this._selectorImage.alt = `Изображение: ${this._name}`;
+    this._setCardTrash();
+    this._isLike();
     this._selectorLike.title = 'Поставить отметку: Нравится';
-    this._selectorCountLike.textContent =`${data.likes.length}`;
-    this._selectorTitle.textContent = data.name;
-    this._setEventListeners(this._selectorLike, this._selectorImage, this._selectorCountLike, data.link, data.name, data._id);
+    this._selectorCountLike.textContent =`${this._likes}`;
+    this._selectorTitle.textContent = this._name;
+    this.setEventListeners();
     return this._card;
   }
 
-  /* метод: установка просушивателя удаления карточки */
-  _setEventCardTrash(card, selectorTrash, cardId){
-    selectorTrash.addEventListener('click', () => getAgreement(card, cardId));
-  }
-
-  /* метод: поставить или убрать лайк */
-  _toggleLike(selectorLike, selectorCountLike, cardId){
-    if (selectorLike.classList.contains('cards__like_active')){
-      api.deletePullLike(cardId)
-      .then(data => {
-        selectorLike.classList.remove('cards__like_active');
-        selectorCountLike.textContent = `${data.likes.length}`;
-      })
-      .catch(err => console.log(err));
+  /* метод: установка корзинки удаления карточки и просушивателя удаления карточки */
+  _setCardTrash(){
+     if(this._ownerId === this._personId){
+      this._selectorTrash.alt ='Изображение: Корзина';
+      this._selectorTrash.title ='Удалить карточку';
+      this._selectorTrash.src = this.trash;
+      this._selectorTrash.addEventListener('click', () => this._popupConfirmDelete(this._card, this._cardId)); // вешаем прослушиватель на удаления карточки
     } else {
-      api.putPullLike(cardId)
-      .then(data => {
-        selectorLike.classList.add('cards__like_active');
-        selectorCountLike.textContent = `${data.likes.length}`;
-      })
-      .catch(err => console.log(err));
+    this._selectorTrash.remove();  // если карточка не наша - удаляем элемент корзинки из html разметки карточки
     }
   }
 
+  /* метод: проверка наличия отметки "нравится" - поставленной ранее пользователем */
+  _isLike(){
+      for(let i=0; i < this._likes.length; i++){
+      if(this._likes[i]._id === this._personId){
+        this._selectorLike.classList.add('cards__like_active'); // если ранее ставили лайк на карточке - ставим лайк
+      }
+    }
+  }
+
+  /* метод: поставить лайк */
+  putCardLike(){
+     this_.selectorLike.classList.add('cards__like_active');
+     this._selectorCountLike.textContent = `${this._likes.length}`;
+  }
+
+  /* метод: убрать лайк */
+  dellCardLike(){
+     this._selectorLike.classList.remove('cards__like_active');
+     this._selectorCountLike.textContent = `${this._likes.length}`;
+  }
 
    /* метод: установка общих прослушивателей для всех карточек - при клике на сердечко - поставить или убрать лайк,
    при клике на изображение карточки - развернуть изображение карточки  */
-  _setEventListeners(selectorLike, selectorImage, selectorCountLike, cardLink, cardName, cardId) {
-    selectorLike.addEventListener('click', () => toggleLike(selectorLike, selectorCountLike, cardId));
-    selectorImage.addEventListener('click', () => {
-      openPopup(imageModal);
-      picture.src = cardLink;
-      picture.alt = cardName;
-      pictureName.textContent = cardName;
-    });
+  setEventListeners() {
+    this._selectorLike.addEventListener('click', () => this._handleClickLikeCard(this._selectorLike, this._selectorCountLike, this._cardId));
+    this._selectorImage.addEventListener('click', () => this._handleOpenImgCardClick(this._link, this._name));
   }
-  // метод: удалить карточку
-  _removeCard(){
-    this._card.remove();
 
+  // метод: удалить карточку
+  removeCard(){
+    this._card.remove();
   }
 }
